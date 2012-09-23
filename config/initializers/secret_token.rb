4a143e9fd78f66876fc422ae62338fc3208f9756
Scott Weisman <sweisman18@gmail.com>
2012-09-23 00:17:50 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tinypost::Application.config.secret_token = '2213d719b7b69055e75a8c7bd22db85023182e108727b9d408c251487604b3538c2e1be7eaaed7655d363aacb25c0b5e24114f680a34991ee3037f1632b5eeaf'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Tinypost::Application.config.session_store :cookie_store, key: '_tinypost_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Tinypost::Application.config.session_store :active_record_store
