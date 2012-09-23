24d95d42f9fc0140e35e9405475131937f0ad91e
Scott Weisman <sweisman18@gmail.com>
2012-09-23 01:25:13 -0500

    finished static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,45 @@
+require 'spec_helper'
+
+describe "Static pages" do
+
+  describe "Home page" do
+
+    it "should have the content 'TinyPost'" do
+      visit '/static_pages/home'
+      page.should have_content('TinyPost')
+    end
+
+    it "should have the right title text" do
+    	visit '/static_pages/home'
+    	page.should have_selector('title', text: 'TinyPost | Home')
+    end
+  end
+
+  describe "Help page" do
+
+  	it "should have the content 'Help'" do
+  		visit '/static_pages/help'
+  		page.should have_content('Help')
+  	end
+
+  	 it "should have the right title text" do
+    	visit '/static_pages/help'
+    	page.should have_selector('title', text: 'TinyPost | Help')
+    end
+  end
+
+  describe "About" do
+
+  	it "should have content 'About" do
+  		visit '/static_pages/about'
+  		page.should have_content('About')
+  	end
+
+  	it "should have the right title text" do
+    	visit '/static_pages/about'
+    	page.should have_selector('title', text: 'TinyPost | About')
+    end
+  end
+
+
+end
